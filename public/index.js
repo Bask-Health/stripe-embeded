2cc44c52ee5f5b07720fa5b2344625a931eb6653
Leonardo Silva <leonardosilva@v.stockx.com>
2025-01-03 17:07:22 -0300

    account session GET

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ import { loadConnectAndInitialize } from "@stripe/connect-js";
 const instance = loadConnectAndInitialize({
   publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   fetchClientSecret: async () => {
-    const response = await fetch(`${window.location.origin}/api/account_session`, { method: "POST" });
+    const response = await fetch(`${window.location.origin}/api/account_session`, { method: "GET" });
     if (!response.ok) {
       const { error } = await response.json();
       document.querySelector("#container").setAttribute("hidden", "");
